cb1ce9838753ddfc31056eb7f00a4c77b2c43fa6
Sarah <Akhtar>
2023-03-09 18:32:21 -0800

    Initial Commit

---
+++ Zimi/ZimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZimiApp.swift
+//  Zimi
+//
+//  Created by Sarah Akhtar on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ZimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
