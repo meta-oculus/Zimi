5ae37f8d1ce1b0d51613e9e7bb1ed24a00250c2d
Meta Oculus <127272701+meta-oculus@users.noreply.github.com>
2023-03-11 21:01:22 -0800

    Add files via upload

---
+++ Zimi/View/ZMContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Zimi
+//
+//  Created by Sarah Akhtar on 3/9/23.
+//
+
+import SwiftUI
+
+struct ZMContentView : View {
+    var body: some View {
+        VStack {
+            RMCalculatorBMI()
+        }
+    }
+}
+
+struct RMContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZMContentView()
+    }
+}
